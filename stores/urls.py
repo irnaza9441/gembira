6a7ce4c6aaca72abc87db592c341f84f0f4916e5
Aaliya Cassoobhoy <aaliyacassoobhoy@lawn-128-61-19-63.lawn.gatech.edu>
2025-11-06 19:15:16 -0500

    user story 6

--- stores/urls.py
+++ stores/urls.py
@@ -2,4 +2,9 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('cafe', views.cafe, name='stores.cafe'),
+    path('cart/', views.cart, name='stores.cart'),
+    path('cart/clear/', views.clear_cart, name='stores.clear_cart'),
+    path('add/<int:id>/', views.add_to_cart, name='stores.add_to_cart'),
+    path('update/<int:id>/', views.update_cart_item, name='stores.update_cart_item'),
+    path('remove/<int:id>/', views.remove_cart_item, name='stores.remove_cart_item'),
 ]
